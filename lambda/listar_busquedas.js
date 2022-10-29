0699c8b1575ff4d6e386e382055d4e40ffbe1d55
Bensas <juanbensa@gmail.com>
2022-10-29 14:16:01 -0300

    Updated listar_busquedas lambda

--- lambda/listar_busquedas.js
+++ lambda/listar_busquedas.js
@@ -1,24 +1,17 @@
-const AWS = require('aws-sdk');
-const docClient = new AWS.DynamoDB.DocumentClient();
-
-const params = {
-  TableName : 'busquedas'
-}
-
-async function listItems(){
-  try {
-    const data = await docClient.scan(params).promise()
-    return data
-  } catch (err) {
-    return err
-  }
-}
-
 exports.handler = async (event, context) => {
-  try {
-    const data = await listItems()
-    return { body: JSON.stringify(data) }
-  } catch (err) {
-    return { error: err }
+  return {
+    "isBase64Encoded": false,
+    "statusCode": 200,
+    "headers": { },
+    "body": JSON.stringify({ busquedas: [
+      {
+        title: 'Administrador de base de datos SQL',
+        description: 'Alta, baja y modificación de información en una base de datos PostgreSQL. Interfaz adminer. Por lo menos 2 años de experiencia en administración de bases de datos requeridos.'
+      },
+      {
+        title: 'Mesero para pizzería en Villa Urquiza',
+        description: 'Trabajo de lunes a viernes, turno de 18hs a 23hs, no se requiere experiencia previa.'
+      }
+    ]})
   }
 }
